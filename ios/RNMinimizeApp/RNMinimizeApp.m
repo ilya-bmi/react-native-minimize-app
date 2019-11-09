4601bad9aa17b2087f881ff8fe4921ff46b7b221
Ilya Lazarev <ilyalazarev@MacBook-Air-Ilya.local>
2019-11-09 13:51:17 +0300

    fix wor out of main thread

--- ios/RNMinimizeApp/RNMinimizeApp.m
+++ ios/RNMinimizeApp/RNMinimizeApp.m
@@ -11,8 +11,27 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(minimizeApp)
 {
-    UIApplication *app = [UIApplication sharedApplication];
-    [app performSelector:@selector(suspend)];
+  
+      dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+
+
+    dispatch_sync(dispatch_get_main_queue(), ^{
+                    // Update UI
+                    // Example:
+                    // self.myLabel.text = result;
+
+                       UIApplication *app = [UIApplication sharedApplication];
+   [app performSelector:@selector(suspend)];
+                });
+
+        //Run your loop here
+   // NSString *phoneNumber = @"tel://+37167041100";
+   //  NSString *phoneNumber2 = @"tel://+37127701005";
+
+    });
+
+ //   UIApplication *app = [UIApplication sharedApplication];
+ //   [app performSelector:@selector(suspend)];
 };
 
 @end
